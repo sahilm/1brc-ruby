0d2b6595d2ddee251b6dc3180ac45b10b017ddd9
Sahil Muthoo <sahil.muthoo@gmail.com>
2026-01-27 22:20:04 +0700

    marginal speedup

--- v1_1brc.rb
+++ v1_1brc.rb
@@ -29,6 +29,7 @@ module V1
 
       File.open(file, "r:UTF-8") do |f|
         f.each_line do |line|
+          # marginal speedup over splitting the string
           name = line.slice!(0, line.index(";"))
           stations[name].add_measurement(BigDecimal(line[1..]))
         end
